dbe96224d129b133576803356840b2a50eb52be9
Valach Norbert <167793115+norbi212@users.noreply.github.com>
2025-05-21 10:07:41 +0200

    Update contact.js

--- webprogramozas/utazas/js/contact.js
+++ webprogramozas/utazas/js/contact.js
@@ -1,5 +1,6 @@
 document.addEventListener("headerReady",()=>{
     document.querySelector(".pageTitle").innerText="Kapcsolat";
+    document.querySelector(".current").innerText="Kapcsolat";
 });
 document.addEventListener("cityListReady",()=>{
     
