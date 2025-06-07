11db8a96c87c80b4ec96c6313cbc086d1efad81a
NorbiV <norbi.valach@gmail.com>
2025-06-07 15:45:43 +0200

    link feluliras id-hez _teszt

--- novenyek/plants.js
+++ novenyek/plants.js
@@ -1,3 +1,13 @@
 const urlParts = window.location.pathname.split('/');
 const id = urlParts[urlParts.length - 1];
-document.getElementById("id").innerText=id;
+fetch('plants.json')
+      .then(response => response.json())
+      .then(data => {
+        const plant = data[id];
+        document.getElementById('plant-image').src = plant.image;
+        document.getElementById('plant-image').alt = plant.type;
+        document.getElementById('plant-type').textContent = plant.type;
+        document.getElementById('plant-id').textContent = plant.id;
+        document.getElementById('plant-height').textContent = plant.height;
+      })
+      .catch(error => console.error('Error loading plant data:', error));
